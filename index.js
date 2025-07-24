307b1058fb72389f870d0a04f6cfe78f4df259ae
Abdallah Abou Hamdan <abdallahabouhamdan@Abdallahs-MacBook-Air.local>
2025-07-24 22:02:53 +0300

    ex6

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function Callcon(){
-    console.log(42)
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+function totallogs(){
+   let lapstime=lap1 + lap2 +lap3
+    console.log(lapstime)
 }
-Callcon()
+totallogs()
